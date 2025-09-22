c7ef1c3fdf83af3bbf0677ddbb56aefefb96e58f
yuvankumarL <yuvanyuku@gmail.com>
2025-09-22 12:19:06 +0530

    working with urls and dyanmic url segements completed

---
+++ 1hr/workspace/myapp/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # path("post/<int:post_id>/", views.detail, name="detail")   --> this wont take the string values
+    path("post/<str:post_id>/", views.detail, name="detail"),
+    path("new_url/", views.new_url_view, name="new_page_url"),
+    path("old_url/", views.old_url_redirect, name="old_url")
+
+]
---
+++ 1hr/workspace/myapp/blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello World, You are at blog's index")
+    
+def detail(request, post_id):
+    return HttpResponse(f"You are viewing post details page {post_id}")
+
+def old_url_redirect(request):
+    return redirect("new_url")
+
+def new_url_view(request):
+    return HttpResponse("This is the new URL")
