6a1b9e813020915e55de9353d6a8949b6c08f60e
yuvankumarL <yuvanyuku@gmail.com>
2025-09-22 12:47:22 +0530

    static files

--- 1hr/workspace/myapp/myapp/settings.py
+++ 1hr/workspace/myapp/myapp/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    "/blog/static/blog"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
