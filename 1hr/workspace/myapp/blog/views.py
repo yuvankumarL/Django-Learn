14d6c52d2d9e1aaebfd8436cc8bd2f21b319743f
yuvankumarL <yuvanyuku@gmail.com>
2025-09-22 12:35:31 +0530

    return html

--- 1hr/workspace/myapp/blog/views.py
+++ 1hr/workspace/myapp/blog/views.py
@@ -4,10 +4,12 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World, You are at blog's index")
+    # return HttpResponse("Hello World, You are at blog's index")
+    return render(request, 'blog/index.html')
     
 def detail(request, post_id):
-    return HttpResponse(f"You are viewing post details page {post_id}")
+    # return HttpResponse(f"You are viewing post details page {post_id}")
+    return render(request, 'blog/detail.html')
 
 def old_url_redirect(request):
     # return redirect("new_url") 
