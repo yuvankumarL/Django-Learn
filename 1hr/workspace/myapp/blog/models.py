6586c5e040c199cf21da67ae5af8ee7863f58ef9
yuvankumarL <yuvanyuku@gmail.com>
2025-09-23 16:02:49 +0530

    creating model

--- 1hr/workspace/myapp/blog/models.py
+++ 1hr/workspace/myapp/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    img_url = models.URLField(blanl=True) # default max_length=200, blank=True means its a nullabe column
+
+    def __str__(self):
+        return f'{self.title}'
+
