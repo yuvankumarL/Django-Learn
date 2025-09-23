76571899c5072d84472928f8c9ef8ace977dc60b
yuvankumarL <yuvanyuku@gmail.com>
2025-09-23 12:40:02 +0530

    for loop

--- 1hr/workspace/myapp/blog/views.py
+++ 1hr/workspace/myapp/blog/views.py
@@ -6,7 +6,13 @@ from django.urls import reverse
 def index(request):
     # return HttpResponse("Hello World, You are at blog's index")
     blog_title = "Latest Post"
-    return render(request, 'blog/index.html', {'blog_title': blog_title})
+    posts = [
+        {'title': 'Post 1', 'content': 'Content for Post 1'},
+        {'title': 'Post 2', 'content': 'Content for Post 2'},
+        {'title': 'Post 3', 'content': 'Content for Post 3'},
+        {'title': 'Post 4', 'content': 'Content for Post 4'},
+    ]
+    return render(request, 'blog/index.html', {'blog_title': blog_title, 'posts': posts})
     
 def detail(request, post_id):
     # return HttpResponse(f"You are viewing post details page {post_id}")
