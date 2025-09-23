36ad625bf8478120f06f2fa64f0439cb88ca751b
yuvankumarL <yuvanyuku@gmail.com>
2025-09-23 16:19:40 +0530

    creating migrations

--- 1hr/workspace/myapp/blog/models.py
+++ 1hr/workspace/myapp/blog/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    img_url = models.URLField(blanl=True) # default max_length=200, blank=True means its a nullabe column
-
+    img_url = models.URLField(null=True) # default max_length=200, blank=True means its a nullabe column
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f'{self.title}'
 
