592a2b5a0bbc42ed77e27da1041454f1857d63c1
yuvankumarL <yuvanyuku@gmail.com>
2025-09-23 15:55:08 +0530

    connecting MySql database

--- 1hr/workspace/myapp/myapp/settings.py
+++ 1hr/workspace/myapp/myapp/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'yuvan',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
