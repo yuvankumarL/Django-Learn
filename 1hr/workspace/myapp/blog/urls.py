757a7d88aa32395c10111caf6ef25e9a31ab6c64
yuvankumarL <yuvanyuku@gmail.com>
2025-09-24 12:27:13 +0530

    working with contact form

--- 1hr/workspace/myapp/blog/urls.py
+++ 1hr/workspace/myapp/blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # path("post/<int:post_id>/", views.detail, name="detail")   --> this wont take the string values
     path("post/<str:slug>/", views.detail, name="detail"),
     path("new_url/", views.new_url_view, name="new_page_url"),
-    path("old_url/", views.old_url_redirect, name="old_url")
+    path("old_url/", views.old_url_redirect, name="old_url"),
+    path("contact/", views.contact_view, name="contact")
 
 ]
--- 1hr/workspace/myapp/blog/views.py
+++ 1hr/workspace/myapp/blog/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 import logging
 from .models import Post
 from django.core.paginator import Paginator
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -50,3 +51,11 @@ def old_url_redirect(request):
 
 def new_url_view(request):
     return HttpResponse("This is the new URL")
+
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            logger = logging.getLogger("TESTING")
+            logger.debug(f'POST data is {form.cleaned_data['name'], form.cleaned_data['email'], form.cleaned_data['message']}')
+    return render(request, 'blog/contact.html')
